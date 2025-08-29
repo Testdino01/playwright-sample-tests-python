776fe529aec05e2c0c721cdec70df15c79ad5413
testdino <alphabin@Alphabins-Mac-mini.local>
2025-08-29 18:12:11 +0530

    added more test cases

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -590,3 +590,20 @@ test('Verify that all the navbar are working properly', async () => {
     await allPages.homePage.assertAboutUsTitle();
 });
 
+test.only('Verify that user is able to delete selected product from cart', async () => {
+    const productName = 'GoPro HERO10 Black';
+    await login();
+    await allPages.inventoryPage.clickOnShopNowButton();
+    await allPages.inventoryPage.clickOnAllProductsLink();
+    await allPages.inventoryPage.searchProduct(productName);
+    await allPages.inventoryPage.verifyProductTitleVisible(productName);
+    await allPages.inventoryPage.clickOnAddToCartIcon();
+
+    await allPages.cartPage.clickOnCartIcon();
+    await allPages.cartPage.verifyCartItemVisible(productName);
+    await allPages.cartPage.clickOnDeleteProductIcon();
+    await allPages.cartPage.verifyCartItemDeleted(productName);
+    await allPages.cartPage.verifyEmptyCartMessage();
+    await allPages.cartPage.clickOnStartShoppingButton();
+    await allPages.allProductsPage.assertAllProductsTitle();
+});
